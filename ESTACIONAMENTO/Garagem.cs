1e45a00fc6f0ad286879c1306687b6a0883899f2
Dev-Arevalo <111651801+Dev-Arevalo@users.noreply.github.com>
2022-10-09 18:44:38 -0400

    first commit

---
+++ ESTACIONAMENTO/Garagem.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESTACIONAMENTO
+{
+    public class Garagem
+    {
+
+        double precoInicial = 5.00;
+        double precoPorHora = 3.00;
+
+    
+    }
+}
